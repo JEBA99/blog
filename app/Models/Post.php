7e1a0995463269990440050cd7c95b0ecfd827bb
Jebakumar <jebakumars@blackant.io>
2021-11-25 20:17:05 +0530

    Dynamically load each post when click a particular post from the blog of post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -52,10 +52,13 @@ class Post {
     }
 
     public static function find($slug) {
-        if(! file_exists( $path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
 
-        return cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path)); 
+        return static::all()->firstWhere('slug', $slug);
+        // dd($posts->firstWhere('slug', $slug));
+        // if(! file_exists( $path = resource_path("posts/{$slug}.html"))) {
+        //     throw new ModelNotFoundException();
+        // }
+
+        // return cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path)); 
     }
 }
